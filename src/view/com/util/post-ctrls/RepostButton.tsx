86fb38024ccbc3e8e964ec7ecd57fce9cc2d3e21
Samuel Newman <mozzius@protonmail.com>
2024-04-04 02:23:32 +0100

    alf the repost dropdown on web + import icons

--- src/view/com/util/post-ctrls/RepostButton.tsx
+++ src/view/com/util/post-ctrls/RepostButton.tsx
@@ -1,15 +1,16 @@
 import React, {memo, useCallback} from 'react'
 import {StyleProp, StyleSheet, TouchableOpacity, ViewStyle} from 'react-native'
-import {RepostIcon} from 'lib/icons'
-import {s, colors} from 'lib/styles'
-import {useTheme} from 'lib/ThemeContext'
-import {Text} from '../text/Text'
-import {pluralize} from 'lib/strings/helpers'
-import {HITSLOP_10, HITSLOP_20} from 'lib/constants'
-import {useModalControls} from '#/state/modals'
-import {useRequireAuth} from '#/state/session'
 import {msg} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
+
+import {useModalControls} from '#/state/modals'
+import {useRequireAuth} from '#/state/session'
+import {HITSLOP_10, HITSLOP_20} from 'lib/constants'
+import {RepostIcon} from 'lib/icons'
+import {pluralize} from 'lib/strings/helpers'
+import {colors, s} from 'lib/styles'
+import {useTheme} from 'lib/ThemeContext'
+import {Text} from '../text/Text'
 
 interface Props {
   isReposted: boolean
