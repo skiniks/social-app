9a75ba1f270eb05d4ddd9a8c899d0d5344ffbada
Hailey <me@haileyok.com>
2024-04-17 00:54:12 -0700

    some config options

--- src/screens/Login/hooks/useLogin.ts
+++ src/screens/Login/hooks/useLogin.ts
@@ -3,7 +3,6 @@ import * as Browser from 'expo-web-browser'
 
 import {
   DPOP_BOUND_ACCESS_TOKENS,
-  OAUTH_APPLICATION_TYPE,
   OAUTH_CLIENT_ID,
   OAUTH_GRANT_TYPES,
   OAUTH_REDIRECT_URI,
@@ -23,12 +22,21 @@ export function useLogin() {
         response_types: OAUTH_RESPONSE_TYPES,
         scope: OAUTH_SCOPE,
         dpop_bound_access_tokens: DPOP_BOUND_ACCESS_TOKENS,
-        application_type: OAUTH_APPLICATION_TYPE,
+        application_type: 'web',
       },
+      responseMode: 'fragment',
+      plcDirectoryUrl: 'http://localhost:2582',
+      atprotoLexiconUrl: 'http://localhost:2584',
       fetch: global.fetch,
     })
 
-    const url = await oauthFactory.signIn('http://localhost:2583/')
+    const url = await oauthFactory.signIn('http://localhost:2583/', {
+      display: 'page',
+      max_age: 3600,
+      prompt: 'login',
+      scope: OAUTH_SCOPE,
+      state: 'foo',
+    })
 
     console.log(url.href)
 
